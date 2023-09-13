f61db92c233239636601f5d8b67951194130b972
Harikarthik1301 <harikarthik.a@g2techsoft.com>
2023-09-13 15:26:10 +0530

    stream

---
+++ stream.js
@@ -0,0 +1,9 @@
+const fs = require('fs')
+const path = require('path')
+
+const rs = fs.createReadStream(path.join(__dirname,'files','bigfile.txt'))
+const ws = fs.createWriteStream(path.join(__dirname,'files','new_bigfile.txt'))
+
+rs.on ('data',(datachunk)=>{
+    ws.write(datachunk)
+})
