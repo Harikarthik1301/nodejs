02d561897df06b9dae90eb5b96e2c806b3ddddf1
Harikarthik1301 <harikarthik.a@g2techsoft.com>
2023-09-13 12:44:10 +0530

    node

--- server.js
+++ server.js
@@ -4,21 +4,36 @@ console.log(os.type());
 console.log(os.version());
 console.log(os.homedir());
 
-const fs = require('node:fs');
+const fspromises = require('fs').promises;
 const path = require('path');
 
- 
-fs.readFile(path.join(__dirname,'files','start.txt'),'utf8',(err,data)=>{
-    if (err) throw err
-    console.log(data);
-});
+ const filesystem = async () =>{
+   try{
+    const data =  await fspromises.readFile(path.join(__dirname,'files','start.txt'),'utf8')
+        console.log(data)
+    await fspromises.writeFile(path.join(__dirname,'files','vasa.txt'),'vasanth is the marvelous boy in the bca shift 2 of the srcas',
+        console.log("write1 completed")
+    )
+    await fspromises.writeFile(path.join(__dirname,'files','lufffy.txt'),'Monkey D Luffy is the pirate known as King Of Pirates in the anime called one peice.',
+        console.log("write2 completed")
+    )
+    await fspromises.appendFile(path.join(__dirname,'files','lufffy.txt'),'\nMonkey D Luffy has two friends us his subordinates called zoro and sanji.',
+        console.log("apppend completed")
+    )
+   await fspromises.rename(path.join(__dirname,'files','lufffy.txt'),(path.join(__dirname,'files','Onepeice1.txt')),
+       console.log("Rename completed")
+    )
+    await fspromises.unlink(path.join(__dirname,'files','Onepeice1.txt'),
+       console.log("Delete completed")
+    )
+   } catch(err){
+    console.error(err);
+   }
+ }
+//function call
+filesystem();
 
 process.on('uncaughtException',err =>{
     console.error(`uncaught error :${err}`);
     process.exit(1);
 })
-
-fs.writeFile(path.join(__dirname,'files','vasa.txt'),'vasanth is the marvelous boy in the bca shift 2 of the srcas',(err,data)=>{
-    if (err) throw err
-    console.log("completed");
-});
