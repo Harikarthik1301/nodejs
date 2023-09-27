fa47fbfa6b0af78f8b96592c18bcb23bfdf31407
Harikarthik1301 <harikarthik.a@g2techsoft.com>
2023-09-27 12:49:43 +0530

    webserver

--- webserver.js
+++ webserver.js
@@ -1,6 +1,27 @@
+const { log, error } = require('console');
 const http = require('http')
-const PORT = 3000
+const PORT = 7777
+const fs = require('fs')
 
 const webserver = http.createServer((req,res)=>{
-    
+ res.writeHead(200,{'Content-Type':'text/html'})
+ fs.readFile('index.html',(error,data)=>{
+ if (error){
+    res.writeHead(404)
+    res.write('Page Not Found')
+ }
+ else{
+   res.write(data)
+ }
+ res.end()
+ })
+})
+
+webserver.listen(PORT,(error)=>{
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log('server running on port : '+ PORT);
+    }
 })
